f9353a340dec776b3a09dbdd7605cbb6371b50e7
The Vatch (Mac) <alecwmiller@live.co.uk>
2018-04-24 16:49:23 +0100

    Added Splash Screen

---
+++ mobile/src/main/java/com/vatchproductions/vatch/youarewhatyoueat/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.vatchproductions.vatch.youarewhatyoueat;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread splashThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent splashIntent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(splashIntent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        splashThread.start();
+    }
+}
